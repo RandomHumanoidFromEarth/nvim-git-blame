bc74a205e6dc2d938670a4f1880b7bee0dd73028
mosix <olmoanders@icloud.com>
2024-05-18 14:32:10 +0200

    Add window-manager

---
+++ lua/nvim-git-blame/window-manager.lua
@@ -0,0 +1,24 @@
+local M = {}
+
+local win_map = {}
+
+function M.add(buf_blame, buf_code)
+    table.instert(win_map, {
+        buf_blame = buf_blame,
+        buf_code = buf_code
+    }
+end
+
+function M.get(buf_id)
+    for item in win_map:
+        if buf_id == item.buf_blame then
+            return item
+        end
+        if buf_id == item.buf_code then
+            return item
+        end
+    end
+    return nil
+end
+
+return M
