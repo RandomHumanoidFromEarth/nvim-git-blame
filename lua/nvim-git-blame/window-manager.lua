d401b710dd5d71ab5037c4213fd30f30088d1673
mosix <olmoanders@icloud.com>
2024-05-18 17:47:05 +0200

    rewrite window-manager in object-oriented way

--- lua/nvim-git-blame/window-manager.lua
+++ lua/nvim-git-blame/window-manager.lua
@@ -1,24 +1,23 @@
-local M = {}
+local WindowManager = {}
+WindowManager.win_map = {}
 
-local win_map = {}
-
-function M.add(buf_blame, buf_code)
-    table.insert(win_map, {
+function WindowManager:add(buf_blame, buf_code)
+    table.insert(self.win_map, {
         buf_blame = buf_blame,
         buf_code = buf_code
     })
 end
 
-function M.get(buf_id)
-    for item in win_map do
-        if buf_id == item['buf_blame'] then
+function WindowManager:get(buf_id)
+    for _, item in pairs(self.win_map) do
+        if buf_id == item.buf_blame then
             return item
         end
-        if buf_id == item['buf_code'] then
+        if buf_id == item.buf_code then
             return item
         end
     end
     return nil
 end
 
-return M
+return WindowManager
