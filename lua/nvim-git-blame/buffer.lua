6ed2d82ba961ce93bd4b04eac9ac2fde36072f16
mosix <olmoanders@icloud.com>
2024-05-19 14:29:13 +0200

    determine window with

--- lua/nvim-git-blame/buffer.lua
+++ lua/nvim-git-blame/buffer.lua
@@ -1,20 +1,35 @@
 local Buffer = {}
 local api = vim.api
 
-function Buffer.createFromBlames(blames)
+function Buffer:createFromBlames(blames)
+    local o = {}
+    setmetatable(o, { __index = self })
+    o.max_len = 0
     local buf = api.nvim_create_buf(true, true)
     local content = {}
     for _, blame in pairs(blames) do
-        table.insert(content,
-            blame.hash .. " " ..
+        local line = blame.hash .. " " ..
             blame.user .. " " ..
             blame.date .. " " ..
             blame.time .. " " ..
             blame.timezone
-        )
+        table.insert(content, line)
+        local line_len = string.len(line)
+        if line_len > o.max_len then
+            o.max_len = line_len
+        end
     end
     api.nvim_buf_set_lines(buf, 0, -1, false, content)
-    return buf
+    o.buffer = buf
+    return o
+end
+
+function Buffer:getBuffer()
+    return self.buffer
+end
+
+function Buffer:getMaxLen()
+    return self.max_len
 end
 
 return Buffer
