e7b2c79bf5e58d8d54e8713bada1961102c42f60
mosix <olmoanders@icloud.com>
2024-07-23 21:11:20 +0200

    tests: buffer.lua

--- tests/nvim-git-blame/buffer.lua
+++ tests/nvim-git-blame/buffer.lua
@@ -5,42 +5,47 @@ local Buffer = require '../../lua/nvim-git-blame/buffer'
 -- this is the naming schema, class definition
 TestBuffer = {}
 
---function TestBuffer:setUp()
---end
-
-function TestBuffer.tearDown()
-    vim.api = {}
+function TestBuffer:setUp()
+    self.expect = {
+        buffer = nil,
+        from = nil,
+        to = nil,
+        strict_indexing = nil,
+        return_value = nil,
+    }
+    function vim.api.nvim_buf_get_lines(buffer, from, to, strict_indexing)
+        unit.assertEquals(buffer, self.expect.buffer)
+        unit.assertEquals(from, self.expect.from)
+        unit.assertEquals(to, self.expect.to)
+        unit.assertEquals(strict_indexing, self.expect.strict_indexing)
+        return self.expect.return_value
+    end
 end
 
-function TestBuffer.testCreationAndGetId()
+function TestBuffer.TestNew()
     local buf_1 = Buffer:new(0)
     local buf_2 = Buffer:new(1)
     unit.assertEquals(buf_1:getId(), 0)
     unit.assertEquals(buf_2:getId(), 1)
 end
 
-function TestBuffer.TestGetLine()
-    vim.api.nvim_buf_get_lines = function (buffer, from, to, strict_indexing)
-        unit.assertEquals(buffer, 1001)
-        unit.assertEquals(from, to - 1)
-        unit.assertEquals(to, 15)
-        unit.assertEquals(strict_indexing, true)
-        return { "first line" }
-    end
+function TestBuffer:TestGetLine()
+    self.expect.buffer = 1001
+    self.expect.from = 14
+    self.expect.to = 15
+    self.expect.strict_indexing = true
+    self.expect.return_value = { "first line" }
     local buf = Buffer:new(1001)
     local line = buf:getLine(15)
     unit.assertEquals("first line", line)
 end
 
-function TestBuffer.TestGetLines()
-    -- FIXME: function from TestGetLine (above test) is called
-    vim.api.nvim_buf_get_lines = function (buffer, from, to, strict_indexing)
-        unit.assertEquals(buffer, 1003)
-        unit.assertEquals(from, 0)
-        unit.assertEquals(to, -1)
-        unit.assertEquals(strict_indexing, true)
-        return { "line_1", "line_2", "line_3" }
-    end
+function TestBuffer:TestGetLines()
+    self.expect.buffer = 1003
+    self.expect.from = 0
+    self.expect.to = -1
+    self.expect.strict_indexing = true
+    self.expect.return_value = { "line_1", "line_2", "line_3" }
     local buf = Buffer:new(1003)
     local lines = buf:getLines()
     unit.assertEquals(lines[1], "line_1")
